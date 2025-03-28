3d80a37c023ded362d4ee28b9b39c71e8464ec17
daviJoaquim <davijoaquimamerico@gmail.com>
2025-03-28 16:42:38 -0300

    Segunda versão preliminar

--- jogo.js
+++ jogo.js
@@ -1,23 +1,14 @@
+//Moonlander. Um jogo de alunissagem.
+//Davi Joaquim (https://github.com/daviJoaquim)
+//28/03/2025
+//Versão 0.1.0
+
+
 /** @type {HTMLCanvasElement} */
 
+//Seção de Modelagem de dados
 let canvas = document.querySelector("#jogo");
 let contexto = canvas.getContext("2d");
-
-/*contexto.beginPath();
-
-contexto.rect(0, 0, 100, 100);
-contexto.fillStyle = "white";
-contexto.fill();
-contexto.strokeStyle = "black";
-contexto.stroke();
-
-contexto.beginPath();
-contexto.moveTo(100,100);
-contexto.lineTo(200,100);
-contexto.lineTo(100,200);
-contexto.lineTo(100,100);
-contexto.fillStyle = "black";
-contexto.fill();*/
 
 let moduloLunar = {
     posicao: {
@@ -27,14 +18,15 @@ let moduloLunar = {
     angulo: 0,
     largura: 20,
     altura: 20,
-    cor: "black",
+    cor: "lightgray",
     motorLigado: false,
     velocidade: {
         x: 0,
         y: 0
-    }
+    },
+    combustivel: 1000
  }
-    
+    //Seção de Visualização
  function desenharModuloLunar(){
     contexto.save();
     contexto.beginPath();
@@ -59,45 +51,72 @@ let moduloLunar = {
     contexto.moveTo(moduloLunar.largura * -0.5, moduloLunar.altura * 0.5);
     contexto.lineTo(moduloLunar.largura * 0.5, moduloLunar.altura * 0.5);
     //determina o tamanho da chama
-    contexto.lineTo(0, moduloLunar.altura * 0.5 + Math.random() * 20);
+    contexto.lineTo(0, moduloLunar.altura * 0.5 + Math.random() * 100);
     contexto.lineTo(moduloLunar.largura * -0.5, moduloLunar.altura * 0.5);
     contexto.closePath();
     contexto.fillStyle = "orange";
     contexto.fill();
  }
+    function mostrarVelocidade(){
+    contexto.font = "bold 18px Arial";
+    contexto.textAlign = "center";
+    contexto.textBaseline = "middle";
+    contexto.fillStyle = "lightgray";
+    let velocidade = `Velocidade: ${(10 * moduloLunar.velocidade.y).toFixed(2)}`;
+    contexto.fillText(velocidade, 100, 60);
+    }
 
-    let x = 0;
-
+    function mostrarCombustivel(){
+        contexto.font = "bold 18px Arial";
+        contexto.textAlign = "center";
+        contexto.textBaseline = "middle";
+        contexto.fillStyle = "lightgray";
+        let combustivel = `Combustível: ${(moduloLunar.combustivel).toFixed(0)}`;
+        contexto.fillText(combustivel, 300, 60);
+        
+    }
+    function gasto(){
+        if(moduloLunar.combustivel > 0){
+            moduloLunar.combustivel --
+        }else{
+            moduloLunar.combustivel = 0 
+            moduloLunar.motorLigado = false
+        }
+    }
+    
     function desenhar(){
    //limpar tela
    contexto.clearRect(0, 0, canvas.width, canvas.height);
-   
-   contexto.save()//salvando comtexto atual
-   //vai (muda o contexto) para o centro da tela
-   contexto.translate(canvas.width / 2, canvas.height / 2)
-   contexto.beginPath();
-   //contexto.arc(x, 100, 25, 0, 2 * Math.PI )
-   contexto.rotate(Math.PI/4)
-   contexto.rect(x, 100, 25, 10)
-   contexto.fillStyle = "black";
-   contexto.fill();
-   contexto.restore();//restaura o contexto anterior
-
-   x = x + 1;
+   //Esta função atualiza a posição do módulo da função
    atracaoGravitacional();
-   requestAnimationFrame(desenhar);
    desenharModuloLunar();
+   mostrarVelocidade();
+   mostrarCombustivel();
+   //Esta função repete a execução da função desenhar cada atualização a cada quadro
+   if(moduloLunar.posicao.y >= (canvas.height - 0.5 * moduloLunar.altura)) {
+
+    if(moduloLunar.velocidade.y >= 0.5 ){
+        return alert ("VOCÊ MORREU NA QUEDA!");
+    }else{
+         return alert ("VOCÊ CONSEGUIU POUSAR")
+    }
+   }
+   
+   requestAnimationFrame(desenhar);
 }
+    //Seção de controle
 
-
+   //Pressionando a seta para cima para ligar o motor 
  document.addEventListener("keydown", teclaPressionada)
 
  function teclaPressionada(evento){
     if(evento.keyCode == 38){
         moduloLunar.motorLigado = true;
-    }
- }
+        gasto()
+        }
 
+    }
+  
 
  document.addEventListener("keyup", teclaSolta);
  function teclaSolta(evento){
@@ -110,9 +129,20 @@ function atracaoGravitacional(){
     moduloLunar.posicao.x += moduloLunar.velocidade.x;
     moduloLunar.posicao.y += moduloLunar.velocidade.y;
     if(moduloLunar.motorLigado){
-        moduloLunar.velocidade.y -= 0.2;
+        moduloLunar.velocidade.y -= 0.0115;
     }
-    moduloLunar.velocidade.y += gravidade;
-}
 
+    moduloLunar.velocidade.y += gravidade;
+    if(moduloLunar.posicao.y < 10){
+        moduloLunar.posicao.y = 10;
+        moduloLunar.velocidade.y = 0;
+       
+    }
+    if(moduloLunar.posicao.y > 590){
+        moduloLunar.posicao.y = 590;
+        moduloLunar.velocidade.y = 0;
+    }
+
+}
 desenhar();
+
